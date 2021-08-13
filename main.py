22f00b54445fbbc0418c1796140cc20d11950b4d
Fernando <fernandonsc5@gmail.com>
2021-08-12 23:00:52 -0300

    First CORE implementation
    Execute every question step on Data.py and returns an json object with every step response.

---
+++ main.py
@@ -0,0 +1,60 @@
+import sys
+
+from PyInquirer.prompt import prompt
+sys.path.insert(1, 'assets/')
+import Data, Style
+
+"""
+@author: tiannamen (FernandoNSC5)
+12/08/2021
+"""
+class Main():
+
+    def __init__(self):
+        self.data = Data.Data()
+        self.style = Style.Style()
+
+        self.answers = list()
+
+        self.printHeader()
+        self.printQuestion()
+
+    def printHeader(self):
+        print(self.style.RED + self.data.LOGO)
+        print(self.style.RESET + self.data.HEADER)
+        print(self.style.RESET + self.data.BR)
+
+    def printQuestion(self):
+        for step in self.data.STEPS:
+            if(step[1]):
+                question = self.buildListQuestion(step[0], self.data.QUESTIONS[step[0]])
+            else:
+                question = self.buildQuestion(step[0], self.data.QUESTIONS[step[0]])
+
+            self.answers.append(prompt(question, style = self.style.CLI_FORMAT))
+
+    def buildListQuestion(self, step, data):
+        header = data[0]
+        answers = list()
+        for i in range(1, len(data)):
+            answers.append(data[i])
+        return [
+            {
+                'type'      : 'list',
+                'message'   : header,
+                'name'      : step,
+                'choices'   : answers
+            }
+        ]
+
+    def buildQuestion(self, step, data):
+        return [
+            {
+                'type'      : 'input',
+                'message'   : data,
+                'name'      : step
+            }
+        ]
+
+m = Main()
+print(m.answers)
